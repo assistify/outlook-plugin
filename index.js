e1573ee9a613fc03e06282eb1d0f126694069d0e
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-07 10:02:46 +0200

    Serve assets and manifest as well

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ const PORT = process.env.PORT || 3000
 
 connect()
   .use(serveStatic(__dirname + '/dist'))
+  .use(serveStatic(__dirname + '/assets'))
+  .use(serveStatic(__dirname + '/manifest.xml'))
   .listen(PORT, () => logger.info(`Server running on ${PORT}...`))
 
