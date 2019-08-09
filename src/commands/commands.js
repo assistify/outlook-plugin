943d3e8fb9faf92ea100b59109fa34825e3e6fae
Vigneshwaran Odayappan <Vigneshwaran.Odayappan@deutschebahn.com>
2019-08-09 15:19:42 +0200

    Implementation to invoke the UI less command

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
@@ -8,6 +6,40 @@ import $ from 'jquery';
 Office.onReady(info => {
   // If needed, Office.js is ready to be called
 });
+
+function showConfigDialog(event) {
+  // Not Configured: Show the configuration dialog
+  configEvent = event;
+  var url = new URI('../src/settings/login.html').absoluteTo(window.location).toString();
+  var dialogOptions = { width: 20, height: 40, displayInIframe: true };
+  Office.context.ui.displayDialogAsync(url, dialogOptions, function (result) {
+    loginDialog = result.value;
+    loginDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogMessageReceived, processMessage);
+    loginDialog.addEventHandler(Microsoft.Office.WebExtension.EventType.DialogEventReceived, dialogClosed);
+  });
+}
+
+function processMessage(message) {
+  config = JSON.parse(message.message);
+  setConfiguration(config, function (result) {
+    loginDialog.close();
+    loginDialog = null;
+    configEvent.completed();
+    configEvent = null;
+  });
+}
+
+function dialogClosed(message) {
+  loginDialog = null;
+  configEvent.completed();
+  configEvent = null;
+}
+
+
+function isValidConfig(config) {
+  return true;
+  //config && config.server && config.userId && config.authToken;
+}
 
 function getItemRestId() {
   if (Office.context.mailbox.diagnostics.hostName === 'OutlookIOS') {
@@ -22,207 +54,66 @@ function getItemRestId() {
   }
 }
 
-function getCurrentItem(accessToken) {
-  // Get the item's REST ID
-  var itemId = getItemRestId();
-
-  // Construct the REST URL to the current item
-  // Details for formatting the URL can be found at
-  // /previous-versions/office/office-365-api/api/version-2.0/mail-rest-operations#get-a-message-rest
-  var getMessageUrl = Office.context.mailbox.restUrl +
-    '/v2.0/me/messages/' + itemId;
-
-  const item = $.ajax({
-    url: getMessageUrl,
-    dataType: 'json',
-    headers: { 'Authorization': 'Bearer ' + accessToken },
-    async: false,
-  });
-  return item.responseJSON;
-}
-// **********************
-// ROCKETCHAT API CALLS
-// **********************
-
-function login(baseUrl, email, password) {
-  const response = $.ajax({
-    url: baseUrl+ 'login',
-    dataType: 'json',
-    method: 'POST',
-    data: {
-      'email': email,
-      'password': password
-    },
-    async: false,
-  });
-  const userId = response.responseJSON.data.userId
-  const authToken = response.responseJSON.data.authToken
-  return {userId, authToken}
-}
-
-function getRoom(baseUrl, name, {userId, authToken}) {
-  let response = $.ajax({
-    url: baseUrl+'channels.info',
-    dataType: 'json',
-    method: 'GET',
-    headers: {
-      'X-Auth-Token': authToken,
-      'X-User-Id': userId,
-    },
-    data: {
-      'roomName': name
-    },
-    async: false,
-  });
-  console.log(response)
-  if (response.responseJSON.success == true) {
-    response = response.responseJSON.channel
-  }
-  else {
-    response = $.ajax({
-      url: baseUrl+'groups.info',
-      dataType: 'json',
-      method: 'GET',
-      headers: {
-        'X-Auth-Token': authToken,
-        'X-User-Id': userId,
-        'Accept': 'application/json'
-      },
-      data: {
-        'roomName': name
-      },
-      async: false,
-    });
-    response = response.responseJSON.group
-  }
-
-  return response
-}
-
-function getParentRoomMembers(baseUrl, parent, {userId, authToken}) {
-  let requestUrl = baseUrl + 'channels.members';
-  let response = $.ajax({
-    url: requestUrl,
-    dataType: 'json',
-    method: 'GET',
-    headers: {
-      'X-Auth-Token': authToken,
-      'X-User-Id': userId,
-      'Accept': 'application/json'
-    },
-    async: false,
-  });
-  if (response.responseJSON.success === true) {
-    return response.body.members.map(member => {
-        return member.username;
-    });
-  }
-  else {
-    requestUrl = baseUrl + 'groups.members';
-    response = $.ajax({
-      url: requestUrl,
-      dataType: 'json',
-      method: 'GET',
-      headers: {
-        'X-Auth-Token': authToken,
-        'X-User-Id': userId,
-        'Accept': 'application/json'
-      },
-      async: false,
-    });
-    return response.responseJSON.members.map(member => {
-        return member.username;
-    });
-  }
-}
-
-function createNewDiscussion(baseUrl, parentId, name, users, {userId, authToken}) {
-  const response = $.ajax({
-    url: baseUrl+ 'rooms.createDiscussion',
-    dataType: 'json',
-    method: 'POST',
-    headers: {
-      'X-Auth-Token': authToken,
-      'X-User-Id': userId,
-    },
-    data: {
-      'prid': parentId,
-      't_name': name,
-      'users': users
-    },
-    async: false,
-  });
-  return response.responseJSON.discussion
-}
-
-
-function postMail(baseUrl, roomId, text, {userId, authToken}) {
-  const requestUrl = baseUrl + 'chat.postMessage';
-  const response = $.ajax({
-    url: requestUrl,
-    dataType: 'json',
-    method: 'POST',
-    headers: {
-      'X-Auth-Token': authToken,
-      'X-User-Id': userId,
-    },
-    data: {
-      'roomId': roomId,
-      'text': text
-    }
-  });
-
-  return response.responseJSON
-}
 /**
  * Shows a notification when the add-in command is executed.
  * @param event {Office.AddinCommands.Event}
  */
-function action(event) {
+function forward(event) {
 
-  Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
-    if (result.status === "succeeded") {
-      var accessToken = result.value;
-      // Use the access token
-      console.log('get item');
-      const item = getCurrentItem(accessToken)
-      console.log(item)
-      if (item) {
-        // GET PARENT ROOM
-        const url = ''
-        const parent = ''
-        const email = ''
-        const password = ''
+  //Get the configuration for the Add-in.
+  var config = getConfiguration();
 
-        const {userId, authToken} = login(url, email, password);
-        const parentId = getRoom(url, parent, {userId, authToken})._id;
-        
-        // GET USERS
-        // CREATE DISCUSSION
-        const discussion = createNewDiscussion(url, parentId, 'test', [], {userId, authToken});
-        // SEND MESSAGE IN CHAT
-        postMail(url, discussion._id, item.BodyPreview, {userId, authToken})
-        const message = {
-          type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
-          message: "Performed action.",
-          icon: "Icon.80x80",
-          persistent: true
-        }
+  // Configuration must contain the Server Url, UserId & authToken.(if already logged in via api)
+  if (!isValidConfig(config)) {
+    showConfigDialog(event);
+  } else {
 
-        // Show a notification message
-        Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+    // Get the access token
+    Office.context.mailbox.getCallbackTokenAsync({ isRest: true }, function (result) {
+      if (result.status === "succeeded") {
+        var accessToken = result.value;
 
-        // Be sure to indicate when the add-in command function is complete
-        //event.completed();
+        // Get Mail REST Id
+        var itemId = getItemRestId()
+
+        // Read the mail item
+        getItem(accessToken, itemId, function (response, error) {
+          if (error) {
+            showError(error)
+          } else {
+            sendMessage(config, function (response, error) {
+              if (error) {
+                showError(error);
+              } else {
+                const message = {
+                  type: Office.MailboxEnums.ItemNotificationMessageType.InformationalMessage,
+                  message: "Performed action.",
+                  icon: "Icon.80x80",
+                  persistent: true
+                }
+
+                // Show a notification message
+                Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
+
+                // Be sure to indicate when the add-in command function is complete
+                event.completed();
+              }
+            });
+          }
+        });
+      } else {
+        showError();
       }
-    } else {
-      console.log('Error');
-      // Handle the error
-    }
-  });
+    });
 
 
+  }
 }
+
+function showError(error) {
+  console.log(error);
+}
+
 
 function getGlobal() {
   return (typeof self !== "undefined") ? self :
@@ -234,4 +125,4 @@ function getGlobal() {
 const g = getGlobal();
 
 // the add-in command functions need to be available in global scope
-g.action = action;
+g.forward = forward;
