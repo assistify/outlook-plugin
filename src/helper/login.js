e8121038258696bd18f470ec21712b6ef036bcdf
Vigneshwaran Odayappan <Vigneshwaran.Odayappan@deutschebahn.com>
2019-08-12 14:38:18 +0200

    Login helper

---
+++ src/helper/login.js
@@ -0,0 +1,56 @@
+// Invoke the login api call.
+function login(config, callback) {
+  var url = config.server + '/api/v1/login';
+
+  $.ajax({
+    url: url,
+    method: 'POST',
+    data: {
+      user: config.user,
+      password: config.password,
+    },
+  }).done(function (response) {
+    callback(response);
+  }).fail(function (error) {
+    callback(null, error);
+  });
+}
+
+function getJoinedChannels(config, callback) {
+
+  var url = config.server + '/api/v1/channels.list.joined';
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    method: 'GET',
+    headers: {
+      'X-Auth-Token': config.authToken,
+      'X-User-Id': config.userId,
+    },
+  }).done(function (response) {
+    callback(response);
+  }).fail(function (error) {
+    callback(null, error);
+  });
+}
+
+function buildChannelsList(parent, channels, callback) {
+  channels.forEach(function (channel) {
+
+    var list = $('<li>')
+      .appendTo(parent);
+
+    var item = $('<div>')
+      .attr('id', channel._id)
+      .addClass('listitem')
+      .val(channel._id)
+      .appendTo(list);
+
+    var text = $('<label>')
+      .text(channel._id)
+      .appendTo(item);
+
+    item.on('click', { channel: $('#' + channel._id).val() }, callback);
+  });
+
+}
