67021b463ae390a9cc2d12114389f013d01bc22e
Vigneshwaran Odayappan <Vigneshwaran.Odayappan@deutschebahn.com>
2019-08-12 14:37:05 +0200

    Interface to handle the authentication

--- src/settings/login.js
+++ src/settings/login.js
@@ -1,8 +1,13 @@
 (function () {
     'use strict';
-
     // The initialize function must be run each time a new page is loaded.
     Office.initialize = function (reason) {
+
+    };
+
+    $(document).ready(function (e) {
+        var config = {};
+
         $('#server').on('change', function () {
             // TO-DO
         });
@@ -15,24 +20,66 @@
 
         // Handle the connect action on the dialog window.
         $('#connect').on('click', function () {
-            var server = $('#server').val() || 'https://ee144085.ngrok.io';
-            var user = $('#username').val() || 'admin';
-            var password = $('#password').val() || '1234';
+
+            var server = $('#server').val();
+            var user = $('#username').val();
+            var password = $('#password').val();
 
             login({ server: server, user: user, password: password }, function (response, error) {
                 if (error) {
                     showError(error);
                 } else {
                     if (response.status === 'error') {
-                        showError(response);
+                        showError(error);
                     } else {
                         console.log(response);
-                        sendMessageToHost(JSON.stringify({ server: server, userId: response.data.userId, authToken: response.data.authToken }));
+                        config.server = server;
+                        config.userId = response.data.userId;
+                        config.authToken = response.data.authToken;
+                        getJoinedChannels(config, function (response, error) {
+                            if (error) {
+                                showError(error);
+                            } else {
+                                buildChannelsList($('#room-list-picker'), response.channels, onRoomSelected);
+                                showView('#rooms');
+                            }
+                        });
                     }
                 }
             });
-        })
+        });
 
+        function onRoomSelected(e) {
+            console.log(e);
+            config.channel = e.data.channel;
+        }
+
+
+        $('#save').on('click', function (e) {
+            console.log(config);
+            sendMessageToHost(JSON.stringify(config));
+        });
+
+        $('#cancel').on('click', function () {
+            //Logout here..
+            var login = '#login';
+            showView(login);
+        });
+
+        $('#navToLogin').on('click', function () {
+            var login = '#login';
+            showView(login);
+        });
+
+        $('#backToUrl').on('click', function () {
+            var url = '#url';
+            showView(url);
+        });
+
+        function showView(viewName) {
+            $('.view').hide();
+            $(viewName).show();
+        }
 
         // Sends stringified response back to Host from the dialog window.
         function sendMessageToHost(message) {
@@ -43,6 +90,5 @@
             // Handle unexpected error here...
         }
 
-    };
-
+    });
 })();
