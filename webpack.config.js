69fa8bf4265ce7f9c0f604517bdf7a11234ac6e7
Vigneshwaran Odayappan <vickyokrm@gmail.com>
2019-08-13 16:46:32 +0200

    Update web pack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,9 @@ module.exports = async (env, options) => {
     devtool: "source-map",
     entry: {
       polyfill: "@babel/polyfill",
-      "src/taskpane/taskpane": "./src/taskpane/taskpane.js",
-      "src/commands/commands": "./src/commands/commands.js",
-      "src/helper/config":"./src/helper/config.js",
-      "src/helper/login":"./src/helper/login.js",
-      "src/helper/message":"./src/helper/message.js",
-      "src/settings/login":"./src/settings/login.js",
+      "taskpane/taskpane": "./src/taskpane/taskpane.js",
+      "commands/commands": "./src/commands/commands.js",
+      "settings/login":"./src/settings/login.js",
     },
     resolve: {
       extensions: [".ts", ".tsx", ".html", ".js"]
@@ -47,31 +44,37 @@ module.exports = async (env, options) => {
     plugins: [
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        filename: "src/taskpane/taskpane.html",
+        filename: "taskpane/taskpane.html",
         template: "./src/taskpane/taskpane.html",
         chunks: ["polyfill", "taskpane"]
       }),
       new CopyWebpackPlugin([
         {
-          to: "src/taskpane/taskpane.css",
+          to: "taskpane/taskpane.css",
           from: "./src/taskpane/taskpane.css"
         }
       ]),
       new HtmlWebpackPlugin({
-        filename: "src/commands/commands.html",
+        filename: "commands/commands.html",
         template: "./src/commands/commands.html",
         chunks: ["polyfill", "commands"],
         inject: "body"
       }),
       new HtmlWebpackPlugin({
-        filename: "src/settings/login.html",
+        filename: "settings/login.html",
         template: "./src/settings/login.html",
         chunks: ["polyfill", "login"]
       }),
       new CopyWebpackPlugin([
         {
-          to: "src/settings/login.css",
+          to: "settings/login.css",
           from: "./src/settings/login.css"
+        }
+      ]),
+      new CopyWebpackPlugin([
+        {
+          to: "helper/",
+          from: "./src/helper/"
         }
       ]),
     ],
