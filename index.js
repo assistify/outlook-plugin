3e04dd40a6def20ae8a9321ae11e7c2061f9b31c
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-14 16:37:42 +0200

    Removed stuff which is superfluous if served by Github pages

--- index.js
+++
@@ -1,9 +0,0 @@
-const connect = require('connect')
-const serveStatic = require('serve-static')
-const logger = console
-
-const PORT = process.env.PORT || 3000
-
-connect()
-  .use(serveStatic(__dirname + '/dist'))
-  .listen(PORT, () => logger.info(`Server running on ${PORT}...`))
