c3bfa13dcd5e8364c607bb229a0afb929d2a9e84
Thomas Roehl <thomas.t.roehl@deutschebahn.com>
2019-08-28 13:20:15 +0200

    fix path

--- .travis/setupGithubPages.js
+++ .travis/setupGithubPages.js
@@ -1,9 +1,9 @@
 const fs = require('fs')
 
-const messagejs = fs.readFileSync(__dirname + '/src/helper/message.js').toString()
+const messagejs = fs.readFileSync(__dirname + '../src/helper/message.js').toString()
   .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`)
 
-fs.writeFile(__dirname + "/src/helper/message.js", messagejs, (err) => {
+fs.writeFile(__dirname + "../src/helper/message.js", messagejs, (err) => {
     if (err) console.log(err);
     console.log("Successfully Written to File.");
 });
