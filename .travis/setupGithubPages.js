9799bc4f9d85c42d74e1a0839a4b903b68f3cbb3
Thomas Roehl <thomas.t.roehl@deutschebahn.com>
2019-08-28 11:45:38 +0200

    added travis stuff

---
+++ .travis/setupGithubPages.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+
+const messagejs = fs.readFileSync(__dirname + '/src/helper/message.js').toString()
+  .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`)
+
+fs.writeFile(__dirname + "/src/helper/message.js", messagejs, (err) => {
+    if (err) console.log(err);
+    console.log("Successfully Written to File.");
+});
