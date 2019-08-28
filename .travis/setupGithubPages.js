a6043aca0852689fc25a30b9ef6394ebd4777c1e
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-28 15:43:44 +0200

    Resolve file name instead of string concatenation
    This prevents problems with slashes

--- .travis/setupGithubPages.js
+++ .travis/setupGithubPages.js
@@ -1,8 +1,9 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-const messageJsFileName = __dirname + '../src/helper/message.js'
+const messageJsFileName = path.resolve(__dirname, '..', 'src', 'helper', 'message.js';
 const messagejs = fs.readFileSync(messageJsFileName).toString()
-  .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`)
+  .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`);
 
 fs.writeFile(messageJs, messagejs, (err) => {
     if (err) {
