038c732fd6d733b0b5e482124922d4835f78c159
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-28 15:48:16 +0200

    Fixed syntax error
    ...which may occur if editing via the github UI... :-(

--- .travis/setupGithubPages.js
+++ .travis/setupGithubPages.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const messageJsFileName = path.resolve(__dirname, '..', 'src', 'helper', 'message.js';
+const messageJsFileName = path.resolve(__dirname, '..', 'src', 'helper', 'message.js');
 const messagejs = fs.readFileSync(messageJsFileName).toString()
   .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`);
 
