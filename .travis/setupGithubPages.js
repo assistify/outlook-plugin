183cdcd6846ac2afbd319dd9fb98f9b5a435e378
Joachim Schirrmacher <joachim.schirrmacher@deutschebahn.com>
2019-08-28 15:56:35 +0200

    Don't change message.js if logger url is not set

--- .travis/setupGithubPages.js
+++ .travis/setupGithubPages.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 const path = require('path');
 
-const messageJsFileName = path.resolve(__dirname, '..', 'src', 'helper', 'message.js');
-const messagejs = fs.readFileSync(messageJsFileName).toString()
-  .replace(/^var usageLogger = null;/, `var usageLogger = '${process.env.LOGGER_URL}';`);
+const loggerUrl = process.env.LOGGER_URL
 
-fs.writeFile(messageJs, messagejs, (err) => {
+if (loggerUrl) {
+  const messageJsFileName = path.resolve(__dirname, '..', 'src', 'helper', 'message.js');
+  const messagejs = fs.readFileSync(messageJsFileName).toString()
+    .replace(/^var usageLogger = null;/, `var usageLogger = '${loggerUrl}';`);
+
+  fs.writeFile(messageJsFileName, messagejs, (err) => {
     if (err) {
       console.error(err);
     } else {
       console.log('Successfully Written to File.');
     }
-});
+  });
+}
